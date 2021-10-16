fe17e8feb42004abda4090f01d3ec4220e5aa08f
nauski <nauski@nauski.com>
2021-10-16 15:58:31 +0300

    fix: ModuleNotFoundError: Module not found: Error: Can't resolve '../components/UI/ForYouList/ForYouList'

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,5 @@
 import MainLayout from '../components/Layouts/MainLayout'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
-import ForYouList from '../components/UI/ForYouList/ForYouList'
-import JustAdded from '../components/UI/JustAdded/JustAdded'
-import Posterview from '../components/UI/PosterView/PosterView'
-
 
 export default function Home() {
     return (
