76206a3c691505e242182739294ef38403cc44a1
nauski <nauski@nauski.com>
2021-10-19 12:51:57 +0300

    installed axios, MediaRow component now fetches the thumbnail images form tmdb api

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Image from 'next/image'
 import { useState, useEffect } from 'react';
+import axios from 'axios'
+import { shuffleArray } from '../../utilities';
 
 const MediaRow = (props) => {
     const [loadingData, setLoadingData] = useState(true)
+    const [moviesData, setMoviesData] = useState([])
+
+    useEffect(() => {
+        // Make a request for a user with a given ID
+        axios.get(`https://api.themoviedb.org/3/${props.endpoint}&api_key=1c775e4294aadbc6f4ef96ec9024215b`)
+            .then(function (response) {
+                // handle success
+                setMoviesData(shuffleArray(response.data.results))
+                setLoadingData(false)
+                console.log('Success Response For: ', props.title);
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log('Error Response For: ', props.title);
+                console.log(error);
+            })
+
+    }, [])
 
     const loopComp = (comp, digit) => {
         let thumbnails = []
@@ -13,10 +34,12 @@ const MediaRow = (props) => {
         return thumbnails;
     }
     const showThumbnails = () => {
-        setTimeout(() => setLoadingData(false), 3000)
+
         return loadingData
             ? loopComp((<Skeleton />), 10)
-            : loopComp((<Thumbnail />), 10)
+            : moviesData.map((movie) => {
+                return <Thumbnail movieData={movie} />
+            })
     }
     return (
         <div className={`media-row ${props.type}`}>
@@ -28,10 +51,10 @@ const MediaRow = (props) => {
     );
 };
 
-const Thumbnail = () => {
+const Thumbnail = (props) => {
     return (
         <div className="media-row__thumbnail">
-            <img src="https://cdn.shopify.com/s/files/1/0057/3728/3618/products/108b520c55e3c9760f77a06110d6a73b_480x.progressive.jpg?v=1573652543" alt="" />
+            <img src={`https://image.tmdb.org/t/p/original/${props.movieData.poster_path}`} alt="" />
             <div className="media-row__top-layer">
                 <i className="fas fa-play" />
             </div>
